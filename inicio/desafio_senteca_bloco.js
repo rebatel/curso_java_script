eeff89693186f65ed65507d945ce897179e35dab
rebatel <renato.batel@ibm.com>
2023-04-15 07:19:11 -0300

    avanço no curso

---
+++ inicio/desafio_senteca_bloco.js
@@ -0,0 +1,17 @@
+{
+    console.log("Bloco de numeros impares")
+    console.log("Numero #01");
+    console.log("Numero #03");
+    console.log("Numero #05");
+    console.log("Numero #07");
+    console.log("Numero #09");
+}
+
+{
+    console.log("Bloco de numeros pares")
+    console.log("Numero #02");
+    console.log("Numero #04");
+    console.log("Numero #06");
+    console.log("Numero #08");
+    console.log("Numero #10");
+}
