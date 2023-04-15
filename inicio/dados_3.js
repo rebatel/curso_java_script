01823e6a7faf3e7fbc2184e71b482b79123305e7
rebatel <renato.batel@ibm.com>
2023-04-15 08:24:53 -0300

    desafio area

---
+++ inicio/dados_3.js
@@ -0,0 +1,12 @@
+var idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Testes");
+console.log(typeof 'Teste');
---
+++ inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI * raio * raio
+
+let raio = 10;
+let raio2 = raio * raio;
+const PI = 3.141592;
+let areaCirc = PI * raio2;
+
+console.log("O valor da are é: " + areaCirc + "m²");
+
+areaCirc = Math.PI * raio * raio
+console.log("O valor da are é: " + areaCirc + "m²");
