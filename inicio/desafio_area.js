601eaf773fc503cb9a578b49a906a42ac8507d26
rebatel <renato.batel@ibm.com>
2023-04-17 10:42:38 -0300

    atualização de arquivos do curso

--- inicio/desafio_area.js
+++ inicio/desafio_area.js
@@ -1,11 +1,11 @@
 // PI * raio * raio
 
-let raio = 10;
-let raio2 = raio * raio;
+const raio = 10;
 const PI = 3.141592;
-let areaCirc = PI * raio2;
+let areaCirc = PI * raio * raio;
 
 console.log("O valor da are é: " + areaCirc + "m²");
 
 areaCirc = Math.PI * raio * raio
 console.log("O valor da are é: " + areaCirc + "m²");
+
