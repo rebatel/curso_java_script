e67bca9a2a249d12f05c6ca675b468058e41d81e
rebatel <renato.batel@ibm.com>
2023-04-21 15:10:27 -0300

    continuidade do curso

---
+++ exercicios-js/Fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+console.log('Linha 1');
+
+
+/*
+comentario de muitas linhas
+varias linhas
+*/
+console.log('Linha 2');
+
+/* 
+comentario
+de 
+multiplas linhas*/
+
+console.log("linha 3");
---
+++ exercicios-js/Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0);
+console.log("10" / 2); // js le a string e identifica o numero e realiza a divisão
+console.log("show" * 2);
+console.log(0.1 + 0.7);
+//console.log(10.toString());
+console.log((10.345).toFixed(2))
